f61da26c62c48de341d881e93ff76ac4e2cf3ab6
Abigail Turriza <abigailturriza3@gmail.com>
2022-02-20 13:02:08 -0500

    commit intent

--- app/src/main/java/com/example/appclima/Ciudades.kt
+++ app/src/main/java/com/example/appclima/Ciudades.kt
@@ -1,11 +1,28 @@
 package com.example.appclima
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+
+        val bFcp = findViewById<Button>(R.id.bFcp)
+        val bTulum = findViewById<Button>(R.id.bTulum)
+
+        bFcp.setOnClickListener(View.OnClickListener {
+          val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+        })
+        bTulum.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
